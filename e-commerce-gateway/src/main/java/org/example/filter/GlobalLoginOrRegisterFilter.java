a6f156dfff75fca4ee1e81b2578653a4f7199371
zhoudashuai <17691018268@163.com>
2021-12-13 22:37:21 +0800

    update 修改业务逻辑

--- e-commerce-gateway/src/main/java/org/example/filter/GlobalLoginOrRegisterFilter.java
+++ e-commerce-gateway/src/main/java/org/example/filter/GlobalLoginOrRegisterFilter.java
@@ -2,15 +2,12 @@ package org.example.filter;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.cms.PasswordRecipientId;
-import org.checkerframework.checker.units.qual.A;
 import org.example.constant.CommonConstant;
 import org.example.constant.GateWayConstant;
 import org.example.util.TokenParseUtil;
 import org.example.vo.JwtToken;
 import org.example.vo.LoginUserInfo;
 import org.example.vo.UserAndPassword;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
